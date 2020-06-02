b6d42fbe0950f62d88a796731da7d0b56c7cc103
Mariamol James <mariamol.james@gmail.com>
2020-06-02 15:48:26 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,23 @@
-# Write your solution here
+if true
+  puts "This code runs!"
+end
+
+chance_of_rain = 0.2
+if chance_of_rain > 0.5
+  puts "Pack an umbrella!"
+else
+  puts "Enjoy the fine day!"
+end
+
+chance_of_rain = 0.2
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
+end
+
+puts "You know what year it is??"
+this_year = 2019
+puts "Hey, it's 2019!" if this_year == 2019
